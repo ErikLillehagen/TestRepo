99b63b141bfe3539bfdabf0d6f3bccbf18d3fc5f
Notyetnamed <eblillehagen@gmail.com>
2016-04-14 13:44:56 +0200

    fixed broken code

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import testor.studentmodel;
 public class Main {
 
     public static void main(String[] args) {
-        studentmodel studentmodel = new studentmodel("kalle");
-
-        System.out.println("student name" + studentmodel.getName());
+        int string = 12;
     }
 }
