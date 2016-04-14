4890cb2c50885517dfd575ee68a38c6409157200
Notyetnamed <eblillehagen@gmail.com>
2016-04-14 13:18:47 +0200

    added functionality and shit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import testor.studentmodel;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        studentmodel studentmodel = new studentmodel("kalle");
+
+        System.out.println("student name" + studentmodel.getName());
     }
 }
---
+++ src/testor/studentmodel.java
@@ -0,0 +1,20 @@
+package testor;
+
+/**
+ * Created by Erik on 2016-04-14.
+ */
+public class studentmodel {
+    private String name;
+
+    public studentmodel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
