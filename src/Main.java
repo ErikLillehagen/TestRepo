6a0dfd24a484e9aa8df78e1a67d934013c50191c
Notyetnamed <eblillehagen@gmail.com>
2016-04-14 13:26:14 +0200

    ändringar i kod

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         studentmodel studentmodel = new studentmodel("kalle");
 
-        System.out.println("student name" + studentmodel.getName());
+        System.err.println("student name" + studentmodel.getName());
     }
 }
